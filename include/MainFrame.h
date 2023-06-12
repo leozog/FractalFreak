731fcf679660d5579a45af3b481ede0caca8101b
aSliwska <asliwska@student.agh.edu.pl>
2023-06-12 06:07:19 +0200

    uploading my proposal for the base of the project
    base of the project, currently works on manual variable inputs. implemented 2D fractal animation, scaling and centering of fractals included (currently doesn't properly scale and center frames besides the first and last one and we might want to change that). animation is replayable.

---
+++ include/MainFrame.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "GUI.h"
+#include "FractalCalculations.h"
+
+#include <wx/dcbuffer.h>
+#include <wx/colordlg.h>
+
+
+// class implementing user interactions with the GUI
+class MainFrame : public GUIMainFrame
+{
+protected:
+	// plays the animation when playAnimationButton is clicked
+	void playAnimation(wxCommandEvent& event) override;
+
+	// displays wxImage frame
+	void drawFrame();
+
+	FractalCalculations frc_calc; // contains fractals and methods working on fractals
+
+
+public:
+	MainFrame(wxWindow* parent) : GUIMainFrame(parent) {}
+
+};
