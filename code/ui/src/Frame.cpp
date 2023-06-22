54ae5a45d0fb8954fa00ec5c607a13cacc1c88e7
Tavczan <72963196+Tavczan@users.noreply.github.com>
2023-06-22 14:46:29 +0200

    move the animation code to the controller

--- code/ui/src/Frame.cpp
+++ code/ui/src/Frame.cpp
@@ -58,9 +58,34 @@ void Frame::on_dimension_pick(wxCommandEvent& event)
 
 void Frame::onAnimateButton(wxCommandEvent& event)
 {
-	FractalGenerator generator;
-	for (int i = 0; i < _fractalControls.size(); i++)
-	{
-		generator.addFractal(_fractalControls[i].exportTransforms());
-	}
+	// init
+	// frc_calc.calcSize(0);
+	// frc_calc.calcSize(1);
+	// frc_calc.calcChange();
+
+	// drawing and calculating loop
+	// for (int i = 0; i < frc_calc.get_frames_amount() + 1; i++)
+	// {
+	//	frc_calc.createBitmap();
+	//	drawFrame();
+	//	frc_calc.updateTransformations();
+	// }
+
+	/*
+	wxClientDC dc1(drawPanel);
+	wxBufferedDC dc(&dc1);
+
+	int width, height;
+	drawPanel->GetSize(&width, &height);
+
+	dc.DestroyClippingRegion();
+	dc.SetClippingRegion(0, 0, width, height);
+
+	// draw white bg
+	dc.SetBackground(*wxWHITE_BRUSH);
+	dc.Clear();
+
+	// draw frame
+	dc.DrawBitmap(wxBitmap(frc_calc.frame), 0, 0);
+	*/
 }
