587576638a6a8e7072fd1a10c83f84bb2e9e0f4d
leozog <leonozog221@gmail.com>
2023-06-28 14:58:51 +0200

    merge after Animation initialization fix

--- src/ff/Animation.cpp
+++ src/ff/Animation.cpp
@@ -33,14 +33,14 @@ std::shared_ptr<wxImage> Animation::get_frame_latest() const
         if (frames[i].is_ready())
             return dynamic_cast<const Frame &>(frames[i]).get_img();
     }
-    return std::make_shared<wxImage>(0, 0); // placeholder
+    return std::make_shared<wxImage>(1, 1); // placeholder
 }
 
 std::shared_ptr<wxImage> Animation::get_frame_x(uint32_t x) const
 {
     if (frames[x].is_ready())
         return dynamic_cast<const Frame &>(frames[x]).get_img();
-    return std::make_shared<wxImage>(0, 0);
+    return std::make_shared<wxImage>(1, 1);
 }
 
 uint32_t Animation::n_frames() const
--- src/ui/MainFrame.cpp
+++ src/ui/MainFrame.cpp
@@ -100,175 +100,159 @@ void MainFrame::onAnimateButton(wxCommandEvent &event)
 
 void MainFrame::onGenerateButton(wxCommandEvent &event)
 {
+	// Dobierz FPS tak �eby animacja trwa�a tyle ile trzeba wzgl�dem ilo�ci klatek w transformacjach
+	int totalFrames = 0;
+	for (int i = 0; i < _fractalControls.size(); i++)
+	{
+		totalFrames += _fractalControls[i]._framesToNext;
+	}
+
+	double time;
+	m_timetxt->GetValue().ToDouble(&time);
+
+	double fps = totalFrames / time;
+
 	// inicjalizajca sciezki animacji
 	std::unique_ptr<AnimationPath> path = std::make_unique<AnimationPath>();
 
-	void MainFrame::onGenerateButton(wxCommandEvent & event)
+	// Buduje wektor Transform_2D eksportuj�c dane z UI
+	for (int i = 0; i < _fractalControls.size(); i++)
 	{
-
-		/*
-			Cz�� �aduj�ca fraktal do AppData
-		*/
-
-		std::unique_ptr<AnimationPath> path = std::make_unique<AnimationPath>();
-
-		int totalFrames = 0;
-		for (int i = 0; i < _fractalControls.size(); i++)
-		{
-			totalFrames += _fractalControls[i]._framesToNext;
-		}
-
-		double time;
-		m_timetxt->GetValue().ToDouble(&time);
-
-		// Dobierz FPS tak �eby animacja trwa�a tyle ile trzeba wzgl�dem ilo�ci klatek w transformacjach
-		double fps = totalFrames / time;
-
-		// Buduje wektor Transform_2D eksportuj�c dane z UI
-		for (int i = 0; i < _fractalControls.size(); i++)
-		{
-			std::vector<Transform_2D> transforms = _fractalControls[i].exportTransforms2D();
-			path->add(std::make_unique<simple_fractal::Parameters>(simple_fractal::Parameters(transforms)), 2.0);
-			if (transforms.size())
-				path->add(std::make_unique<simple_fractal::Parameters>(simple_fractal::Parameters(transforms)), _fractalControls[i]._framesToNext);
-			else
-				break; // Ostatni fraktal w UI bez transformacji to koniec animacji, lub je�eli sko�cz� si� fraktale
-		}
-
-		// czas w sekundach od poprzedniego stage'a
-		std::unique_ptr<FractalGenerator::Points> points = std::make_unique<simple_fractal::points_generator>(20000); // w tym wypadku argumentem jest ilosc iteracji
-		std::unique_ptr<FractalGenerator::Pixels> pixels = std::make_unique<simple_fractal::pixels_generator>();
-
-		// inicjalizacja animacji
-		data.animation = std::make_unique<Animation>(
-			std::move(path),   // AnimationPath
-			std::move(points), // Inheritance of FractalGenerator::Points
-			std::move(pixels)  // Inheritance of FractalGenerator::Pixels
-							   // TODO: post_process_stack
-		);
-
-		// get animation resolution
-		int width, height;
-		m_widthtxt->GetValue().ToInt(&width);
-		m_heighttxt->GetValue().ToInt(&height);
-
-		m_fractalPanel->SetSize(width, height);
-		bPanelSizer->SetMinSize(width, height);
-		this->Layout();
-		this->Fit();
-
-		int iterations;
-		m_itertxt->GetValue().ToInt(&iterations);
-
-		std::unique_ptr<FractalGenerator::Points> points = std::make_unique<simple_fractal::points_generator>(iterations); // w tym wypadku argumentem jest ilosc iteracji
-		std::unique_ptr<FractalGenerator::Pixels> pixels = std::make_unique<simple_fractal::pixels_generator>();
-
-		data.animation = std::make_unique<Animation>(std::move(path), std::move(points), std::move(pixels)); // Tworzony nowy obiekt, nadpisuje poprzedni� animacj�, pami�� jest rzekomo zwalniania ~ cz�owiek UI
-
-		try
-		{
-			data.animation->render(fps, -1, width, height); // fps, n_of_threads, W, H
-		}
-		catch (const std::exception &e)
-		{
-			std::cerr << e.what() << '\n';
-		}
+		std::vector<Transform_2D> transforms = _fractalControls[i].exportTransforms2D();
+		if (transforms.size())
+			path->add(std::make_unique<simple_fractal::Parameters>(simple_fractal::Parameters(transforms)), _fractalControls[i]._framesToNext);
+		else
+			break; // Ostatni fraktal w UI bez transformacji to koniec animacji, lub je�eli sko�cz� si� fraktale
 	}
+	// get animation resolution
+	int width, height;
+	m_widthtxt->GetValue().ToInt(&width);
+	m_heighttxt->GetValue().ToInt(&height);
 
-	void MainFrame::drawFrame(std::shared_ptr<wxImage> img)
+	m_fractalPanel->SetSize(width, height);
+	bPanelSizer->SetMinSize(width, height);
+	this->Layout();
+	this->Fit();
+
+	// get iterations
+	int iterations;
+	m_itertxt->GetValue().ToInt(&iterations);
+
+	std::unique_ptr<FractalGenerator::Points> points = std::make_unique<simple_fractal::points_generator>(iterations); // w tym wypadku argumentem jest ilosc iteracji
+	std::unique_ptr<FractalGenerator::Pixels> pixels = std::make_unique<simple_fractal::pixels_generator>();
+
+	// Animation initialization
+	data.animation = std::make_unique<Animation>(
+		std::move(path),   // AnimationPath
+		std::move(points), // Inheritance of FractalGenerator::Points
+		std::move(pixels)  // Inheritance of FractalGenerator::Pixels
+						   // TODO: post_process_stack
+	);
+
+	try
 	{
-		// init
-		wxClientDC dc1(m_fractalPanel);
-		wxBufferedDC dc(&dc1);
-
-		int width, height;
-		m_fractalPanel->GetSize(&width, &height);
-
-		dc.DestroyClippingRegion();
-		dc.SetClippingRegion(0, 0, width, height);
-
-		// draw white bg
-		dc.SetBackground(*wxBLACK_BRUSH);
-		dc.Clear();
-
-		// draw frame
-		dc.DrawBitmap(wxBitmap(*img), 0, 0);
+		data.animation->render(fps, -1, width, height); // fps, n_of_threads, W, H
 	}
-
-	void MainFrame::onFileLoad(wxCommandEvent & event)
+	catch (const std::exception &e)
 	{
-		wxFileDialog
-			openFileDialog(this, _("Otworz plik tekstowy z fraktalem"), "", "",
-						   "TXT z fraktalem (*.txt)|*.txt", wxFD_OPEN | wxFD_FILE_MUST_EXIST);
-		if (openFileDialog.ShowModal() == wxID_CANCEL)
-			return; // the user changed idea...
+		std::cerr << e.what() << '\n';
+	}
+}
 
-		std::ifstream input;
-		input.open(openFileDialog.GetPath().ToStdString());
+void MainFrame::drawFrame(std::shared_ptr<wxImage> img)
+{
+	// init
+	wxClientDC dc1(m_fractalPanel);
+	wxBufferedDC dc(&dc1);
 
-		std::string firstLine;
-		getline(input, firstLine);
+	int width, height;
+	m_fractalPanel->GetSize(&width, &height);
 
-		std::stringstream processing(firstLine);
+	dc.DestroyClippingRegion();
+	dc.SetClippingRegion(0, 0, width, height);
 
-		std::string elem;
-		getline(processing, elem, ',');
+	// draw white bg
+	dc.SetBackground(*wxBLACK_BRUSH);
+	dc.Clear();
 
-		m_widthtxt->SetValue(elem);
+	// draw frame
+	dc.DrawBitmap(wxBitmap(*img), 0, 0);
+}
 
-		getline(processing, elem, ',');
-		m_heighttxt->SetValue(elem);
+void MainFrame::onFileLoad(wxCommandEvent &event)
+{
+	wxFileDialog
+		openFileDialog(this, _("Otworz plik tekstowy z fraktalem"), "", "",
+					   "TXT z fraktalem (*.txt)|*.txt", wxFD_OPEN | wxFD_FILE_MUST_EXIST);
+	if (openFileDialog.ShowModal() == wxID_CANCEL)
+		return; // the user changed idea...
 
-		getline(processing, elem, ',');
-		m_itertxt->SetValue(elem);
+	std::ifstream input;
+	input.open(openFileDialog.GetPath().ToStdString());
 
-		getline(processing, elem, ',');
-		int dimensions = std::atoi(elem.data());
+	std::string firstLine;
+	getline(input, firstLine);
 
-		_fractalControls[_currentFractal].Hide();
-		_fractalControls.clear();
-		_currentFractal = 0;
+	std::stringstream processing(firstLine);
 
-		if (dimensions == 0)
+	std::string elem;
+	getline(processing, elem, ',');
+
+	m_widthtxt->SetValue(elem);
+
+	getline(processing, elem, ',');
+	m_heighttxt->SetValue(elem);
+
+	getline(processing, elem, ',');
+	m_itertxt->SetValue(elem);
+
+	getline(processing, elem, ',');
+	int dimensions = std::atoi(elem.data());
+
+	_fractalControls[_currentFractal].Hide();
+	_fractalControls.clear();
+	_currentFractal = 0;
+
+	if (dimensions == 0)
+	{
+
+		getline(input, elem); // pomi� pozycj� kamery
+		getline(input, elem);
+		int fractals = std::atoi(elem.data());
+		for (int i = 0; i < fractals; i++)
 		{
-
-			getline(input, elem); // pomi� pozycj� kamery
 			getline(input, elem);
-			int fractals = std::atoi(elem.data());
-			for (int i = 0; i < fractals; i++)
+			int transforms = std::atoi(elem.data());
+			_fractalControls.push_back(ControlSet(bTransformHolder, 2, true));
+			for (int j = 0; j < transforms; j++)
 			{
-				getline(input, elem);
-				int transforms = std::atoi(elem.data());
-				_fractalControls.push_back(ControlSet(bTransformHolder, 2, true));
-				for (int j = 0; j < transforms; j++)
+				_fractalControls[i].AddLine();
+
+				getline(input, elem); // transformacja
+				std::stringstream transform(elem);
+
+				std::vector<std::string> inputs;
+				for (int k = 0; k < 6; k++)
 				{
-					_fractalControls[i].AddLine();
-
-					getline(input, elem); // transformacja
-					std::stringstream transform(elem);
-
-					std::vector<std::string> inputs;
-					for (int k = 0; k < 6; k++)
-					{
-						getline(transform, elem, ' ');
-						inputs.push_back(elem);
-					}
-
-					_fractalControls[i].setLine(inputs, j);
+					getline(transform, elem, ' ');
+					inputs.push_back(elem);
 				}
 
-				getline(input, elem);
-				_fractalControls[i]._framesToNext = std::atoi(elem.data()); // frames
+				_fractalControls[i].setLine(inputs, j);
+			}
 
-				if (i == 0)
-				{
-					_fractalControls[0].Show();
-					m_frames->SetValue(elem);
-				}
+			getline(input, elem);
+			_fractalControls[i]._framesToNext = std::atoi(elem.data()); // frames
+
+			if (i == 0)
+			{
+				_fractalControls[0].Show();
+				m_frames->SetValue(elem);
 			}
 		}
 	}
+}
 
-	void MainFrame::onFramesText(wxCommandEvent & event)
-	{
-	}
+void MainFrame::onFramesText(wxCommandEvent &event)
+{
+}
